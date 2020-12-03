79abdb57bfc59f1aaccec60e3295fc76c932a0c5
krunalshimpi99 <75433948+krunalshimpi99@users.noreply.github.com>
2020-12-03 20:56:44 +0530

    Add files via upload

---
+++ ex2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <form action="result.php" method="post">
+    <label>Number of bread</label>
+    <input type="text" name="bread"><br>
+
+    <label>number of vadas</label>
+    <input type="text" name="vada"><br>
+
+    <label>number of samosas</label>
+    <input type="text" name="samosa"><br>
+
+    <label>price of vadapav</label>
+    <input type="text" name="vadapav"><br>
+
+    <label>price of samosas</label>
+    <input type="text" name="samosapav"><br>
+    <input type="submit">
+</form>
+</html>
